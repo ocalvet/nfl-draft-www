7566c81a2dbdfbb242d3c81bf8b5793f2d5932dc
Ovidio R. Calvet <calveto@gmail.com>
2018-08-26 06:35:48 -0400

    Add call to get depth-charts for all teams

--- server/index.js
+++ server/index.js
@@ -8,8 +8,18 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.get('/', async (req, res) => {
+app.get('/', (req, res) => {
+  res.end('Welcome to the draft notes API');
+});
+
+app.get('/draft-rankings', async (req, res) => {
   const response = await fetch(`https://www.fantasyfootballnerd.com/service/draft-rankings/json/${process.env.TOKEN}`);
+  const data = await response.json();
+  res.json(data);
+});
+
+app.get('/depth-charts', (req, res) => {
+  const response = await fetch(`https://www.fantasyfootballnerd.com/service/depth-charts/json/${process.env.TOKEN}`);
   const data = await response.json();
   res.json(data);
 });
